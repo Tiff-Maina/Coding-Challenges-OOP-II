99cdef646315ab05ccf0bf62c5293485d4967870
Tiff-Maina <tiff.maina01@gmail.com>
2025-04-29 09:02:53 +0300

    Initial commit

---
+++ CodingChallenge5/MainHello.java
@@ -0,0 +1,9 @@
+package CodingChallenge5;
+
+public class MainHello {
+        public static void main(String args[]){
+            HelloTillStop h1=new HelloTillStop();
+            h1.showHello();
+        }
+    }  
+
