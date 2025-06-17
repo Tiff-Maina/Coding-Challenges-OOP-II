2f80da4a58db1ec784b6683378f52bdbbca600a0
Tiff-Maina <tiff.maina01@gmail.com>
2025-06-17 11:31:46 +0300

    Moved files out of folders and deleted folders

---
+++ MainHello.java
@@ -0,0 +1,9 @@
+// CodingChallenge5
+
+public class MainHello {
+        public static void main(String args[]){
+            HelloTillStop h1=new HelloTillStop();
+            h1.showHello();
+        }
+    }  
+
